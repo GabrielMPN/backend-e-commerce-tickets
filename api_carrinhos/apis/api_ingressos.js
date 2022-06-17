758506ee883dfbeb40ca49d9b366f0935254b9b3
GabrielMPN <pantuffimpn@hotmail.com>
2022-06-17 01:42:00 -0300

    ajuste api_carrinhos

--- api_carrinhos/apis/api_ingressos.js
+++ api_carrinhos/apis/api_ingressos.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const api = axios.create({ 
-    baseURL: `http://${BASE_URL_GATEWAY}/ingressos`,
+    baseURL: `http://${process.env.BASE_URL_GATEWAY}/ingressos`,
     auth: {
         username: 'admin',
         password: 'laranja22'
